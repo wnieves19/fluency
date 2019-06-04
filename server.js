77e9b792ef5d7d3540a0b45441b7d114c40e57fb
Wilfredo Nieves <wnieves@optivon.net>
2019-06-03 23:49:21 -0400

    accept cors

--- server.js
+++ server.js
@@ -11,5 +11,25 @@ app.get('/', function(req, res) {
   res.sendFile('index.html', { root: __dirname });
 });
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'https://fluencyanalysis-backend.herokuapp.com/get_company_data');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
