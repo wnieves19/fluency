cdf1724468a77650f46679e6692cb8659ed2beed
Wilfredo Nieves <wnieves@optivon.net>
2019-06-03 21:51:23 -0400

    fix environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDLtYja06-GwIIo5u_3KUrHYF5SIXFiKq8",
+    authDomain: "fluencyanalysis.firebaseapp.com",
+    databaseURL: "https://fluencyanalysis.firebaseio.com",
+    projectId: "fluencyanalysis",
+    storageBucket: "fluencyanalysis.appspot.com",
+    messagingSenderId: "481677082321"
+  }
 };
