9f7d823a51a1bdf20554525d875ee8e716843e11
Wilfredo Nieves <wnieves@optivon.net>
2019-06-07 15:04:51 -0400

    Show progress loader in company list

--- src/app/company-management/company-list/company-list.component.ts
+++ src/app/company-management/company-list/company-list.component.ts
@@ -14,26 +14,35 @@ export class CompanyListComponent implements OnInit, OnDestroy {
   companiesTable: MatTableDataSource<any[]>;
   displayedColumns: string[] = ['companyName', 'email', 'actions', 'addCompany'];
   subscriptions: Subscription[]=[];
-
+  loading = true;
+  noCompanies = false;
   constructor(private companyService: CompanyService, private router: Router, private route: ActivatedRoute) {
 
     let subscription = this.companyService.companiesObservable
       .subscribe(companiesSnapshot => {
-        this.companiesTable = new MatTableDataSource(this.companyService.companies);
         //If there's only one company, go to its snapshot
-        if(companiesSnapshot.length===1 && this.companyService.selectedCompany===undefined){
-          this.loadCompanyData(this.companyService.companies[0]);
+        // if(companiesSnapshot.length===1 && this.companyService.selectedCompany===undefined){
+        //   this.loadCompanyData(this.companyService.companies[0]);
+        // }else
+          if(companiesSnapshot.length>0){
+          this.companiesTable = new MatTableDataSource(this.companyService.companies);
+          this.loading = false;
+        }else if(this.companyService.companies.length === 0){
+          this.noCompanies = true;
+          this.loading = false;
         }
       });
     this.subscriptions.push(subscription)
   }
 
   private loadCompanyData(company: Company) {
+    this.loading = true;
     let tbSubscription = this.companyService.fetchTrialBalances(company.companyId)
       .subscribe(() => {
           this.companyService.fetchCompanyUsers(company.companyId).subscribe(user=>{
             this.companyService.selectedCompany = company;
             this.router.navigate(['/company-snapshot']);
+            this.loading = true;
           })
         }
       );
